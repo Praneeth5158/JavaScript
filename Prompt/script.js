93b6ce3298087c34b3da00e1ed5f0b2cd4829697
Praneeth5158 <161028719+Praneeth5158@users.noreply.github.com>
2025-10-30 18:32:42 +0530

    Add files via upload

--- Prompt/script.js
+++ Prompt/script.js
@@ -13,9 +13,11 @@ console.log(`${UserName} gaaru ikkadiki vachi koorchondi`);
 let height=prompt("Mee height cheppagalaraa",162);
 let weight=prompt("Mee weight cheppagalaraa",66);
 let heightinMeters = height/100;
-let BMI= (weight / heightinMeters **2).toFixed(2);
+let BMI = Number((weight / heightinMeters **2).toFixed(2));
 
 console.log(`mee height emo ${height}`);
 console.log(`mee wieght emo ${weight}`);
 console.log(`height in meters is ${heightinMeters}`);
 console.log(`your BMI is ${BMI}`);
+
+BMI < 18.5 ? console.log("underweight") : BMI >=25 ? console.log("Fat") : console.log("Normal");
