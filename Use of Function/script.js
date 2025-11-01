67e02b692e659ec0feeb909a8951795a2f404768
Praneeth5158 <161028719+Praneeth5158@users.noreply.github.com>
2025-11-01 19:54:11 +0530

    Add files via upload

---
+++ Use of Function/script.js
@@ -0,0 +1,16 @@
+function Message(){
+    console.log("Please like and subscribe");
+    console.log( "Thanks for watching ");
+    console
+}
+praneethmsg=prompt("[1]Your name please");
+console.log(`Hi ${praneethmsg} `);
+Message();
+
+satyamsg=prompt("[2]Please enter your Name: ");
+console.log(`Hi ${satyamsg} `);
+Message();
+
+deepumsg=prompt("[3]Please enter your Name: ");
+console.log(`Hi ${deepumsg} `);
+Message();
