3c470a56e3d24f2fa5ace6d6ed924bb034b3f9e4
Praneeth5158 <161028719+Praneeth5158@users.noreply.github.com>
2025-11-01 20:40:58 +0530

    Update script.js

--- Use of Function/script.js
+++ Use of Function/script.js
@@ -1,4 +1,4 @@
-function Message(){
+/*function Message(){
     console.log("Please like and subscribe");
     console.log( "Thanks for watching ");
     console
@@ -13,4 +13,36 @@ Message();
 
 deepumsg=prompt("[3]Please enter your Name: ");
 console.log(`Hi ${deepumsg} `);
+Message();
+
+*/
+
+//sum of ages
+
+function Message(){
+    console.log("Thank you for answering ");
+    console.log("Be happy");
+}
+let agesum=0;
+username=prompt("Enter your name : ");
+age=Number(prompt("enter you age: "))
+console.log(`Hi ${username} your age is ${age}`);
+agesum = agesum+age;
+console.log(`current age is ${agesum}`)
+Message();
+
+
+username=prompt("Enter your name : ");
+age=Number(prompt("enter you age: "))
+console.log(`Hi ${username} your age is ${age}`);
+agesum = agesum+age;
+console.log(`current age is ${agesum}`)
+Message();
+
+
+username=prompt("Enter your name : ");
+age=Number(prompt("enter you age: "))
+console.log(`Hi ${username} your age is ${age}`);
+agesum = agesum+age;
+console.log(`current age is ${agesum}`)
 Message();
