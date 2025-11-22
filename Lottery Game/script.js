74fcd9738a13528567b61bfdea86a04bc1ed1c17
Yamanuri Praneeth <161028719+Praneeth5158@users.noreply.github.com>
2025-11-22 18:42:54 +0530

    Add files via upload

--- Lottery Game/script.js
+++ Lottery Game/script.js
@@ -1,6 +1,8 @@
 const ticket=document.getElementById("pick_number");
 let result=document.getElementById("result");
 let lotterySheetContainer = document.getElementById("lotterySheetContainer");
+let tick=new Audio('tap.wav');
+let win = new Audio('win.wav');
 
 let gifts=[
     "₹100 Cash",
@@ -58,15 +60,43 @@ let gifts=[
 
 
 ticket.addEventListener("click",function(){
-    let randnum = Math.random();
-    let floornum = Math.floor((randnum*50)+1);
-    result.innerHTML = floornum;
-    item.innerHTML = `Congratulations You Got "${gifts[floornum-1]}"`;
+    for (let i=1;i<=50;i++){
+        document.getElementById(i).classList.remove("winning-box");
+    }
+    item.innerHTML = "please wait...."
+    // setTimeout(function(){
+       
+    // },5000);
+    let secondcount =0;
+    let intervalId = setInterval(function(){
+        tick.play();
+        const randomBox = Math.floor(Math.random()*50)+1;
+        console.log(randomBox);
+        secondcount++;
+        for (let j=1;j<=50;j++){
+            if(j===randomBox){
+                document.getElementById(j).classList.add("highlighted-box");
+            }
+            else{
+                document.getElementById(j).classList.remove("highlighted-box");
+            }
+        }
+    
+        if (secondcount === 5){
+            let randnum = Math.random();
+            let floornum = Math.floor((randnum*50)+1);
+            item.innerHTML = `You have got ${floornum} Congratulations You Won "${gifts[floornum-1]}"`;
+            document.getElementById(randomBox).classList.remove("highlighted-box");
+            document.getElementById(floornum).classList.add("winning-box");
+            clearInterval(intervalId);
+            win.play();
 
-})
+        }
+    },1000);
+});
 
 gifts.forEach(function(value,i){
-    let boxelement=`<div class="Box">${i+1}. ${value}<div>`
+    let boxelement=`<div class="Box" id=${i+1}>${i+1}. ${value}<div>`
     console.log(boxelement);
     lotterySheetContainer.insertAdjacentHTML("beforeend",boxelement);
 })
